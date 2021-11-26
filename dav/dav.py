f5881bf3fafe3d89a007bef7fe88eb6355ba3c61
dzenaut <dennis.ledwon@rwth-aachen.de>
2021-11-27 03:12:10 +0900

    colors map according to selected criterion

--- dav/dav.py
+++ dav/dav.py
@@ -13,7 +13,4 @@ def index():
 def data():
     # use pandas for loading csv so that entries have the correct type
     data = pd.read_csv('./data/happiness.csv')
-    # convert to list of lists
-    data = [list(data.columns)] + data.to_numpy().tolist()
-
-    return json.dumps(data)
+    return json.dumps(json.loads(data.to_json(orient='records')))
