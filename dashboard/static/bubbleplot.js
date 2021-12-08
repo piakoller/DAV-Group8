b99bc3af4196ea04651e5c7e277b147f3ff9719f
ru48tak <koller.pia@campus.lmu.de>
2021-12-08 22:48:36 +0900

    changes

--- dashboard/static/bubbleplot.js
+++ dashboard/static/bubbleplot.js
@@ -1,15 +1,15 @@
 // set the dimensions and margins of the graph
-const margin = {top: 60, right: 150, bottom: 60, left: 30},
-    width = 1200 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+const margin = { top: 60, right: 150, bottom: 60, left: 30 },
+  width = 1200 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 const svg = d3.select("#bubble")
   .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+  .attr("transform", `translate(${margin.left},${margin.top})`);
 
 //Read the data
 d3.json('./api/data').then((d) => {
@@ -21,41 +21,41 @@ d3.json('./api/data').then((d) => {
   // Add X axis
   const x = d3.scaleLinear()
     .domain([0, 80000])
-    .range([ 0, width ]);
+    .range([0, width]);
   svg.append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(d3.axisBottom(x).ticks(3));
 
   // Add X axis label:
   svg.append("text")
-      .attr("text-anchor", "end")
-      .attr("x", width)
-      .attr("y", height+50 )
-      .text("Gross National Income(GNI) per Capita in USD");
+    .attr("text-anchor", "end")
+    .attr("x", width)
+    .attr("y", height + 50)
+    .text("Gross National Income(GNI) per Capita in USD");
 
   // Add Y axis
   const y = d3.scaleLinear()
     .domain([70, 85])
-    .range([ height, 0]);
+    .range([height, 0]);
   svg.append("g")
     .call(d3.axisLeft(y));
 
   // Add Y axis label:
   svg.append("text")
-      .attr("text-anchor", "end")
-      .attr("x", 0)
-      .attr("y", -20 )
-      .text("Life Expectancy")
-      .attr("text-anchor", "start")
+    .attr("text-anchor", "end")
+    .attr("x", 0)
+    .attr("y", -20)
+    .text("Life Expectancy")
+    .attr("text-anchor", "start")
 
   // Add a scale for bubble size
   const z = d3.scaleSqrt()
     .domain([3.5, 7.769])
-    .range([ 0, 25]);
+    .range([0, 25]);
 
   // Add a scale for bubble color
   const myColor = d3.scaleOrdinal()
-    .domain([0, 1 , 2, 3, 4])
+    .domain([0, 1, 2, 3, 4])
     .range(d3.schemeSet1);
 
 
@@ -66,30 +66,31 @@ d3.json('./api/data').then((d) => {
   // -1- Create a tooltip div that is hidden by default:
   const tooltip = d3.select("#bubble")
     .append("div")
-      .style("opacity", 0)
-      .attr("class", "tooltip")
-      .style("background-color", "black")
-      .style("border-radius", "5px")
-      .style("padding", "10px")
-      .style("color", "white")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    // .style("background-color", "white")
+    .style("border-radius", "5px")
+    .style("padding", "18pt")
+    .style("font-weight", "bold")
+    .style("color", "black")
 
   // -2- Create 3 functions to show / update (when mouse move but stay on same circle) / hide the tooltip
-  const showTooltip = function(event,d) {
+  const showTooltip = function (event, d) {
     tooltip
       .transition()
       .duration(200)
     tooltip
       .style("opacity", 1)
       .html("Country: " + d.country + ", Happiness: " + d.happiness)
-      .style("left", (event.x)/2 + "px")
-      .style("top", (event.y)/2-50 + "px")
+      .style("left", (event.x) / 2 + "px")
+      .style("top", (event.y) / 2 - 50 + "px")
   }
-  const moveTooltip = function(event, d) {
+  const moveTooltip = function (event, d) {
     tooltip
-      .style("left", (event.x)/2 + "px")
-      .style("top", (event.y)/2-50 + "px")
+      .style("left", (event.x) / 2 + "px")
+      .style("top", (event.y) / 2 - 50 + "px")
   }
-  const hideTooltip = function(event, d) {
+  const hideTooltip = function (event, d) {
     tooltip
       .transition()
       .duration(200)
@@ -102,7 +103,7 @@ d3.json('./api/data').then((d) => {
   // ---------------------------//
 
   // What to do when one group is hovered
-  const highlight = function(event, d){
+  const highlight = function (event, d) {
     // reduce opacity of all groups
     d3.selectAll(".bubbles").style("opacity", .05)
     // expect the one that is hovered
@@ -110,7 +111,7 @@ d3.json('./api/data').then((d) => {
   }
 
   // And when it is not hovered anymore
-  const noHighlight = function(event, d){
+  const noHighlight = function (event, d) {
     d3.selectAll(".bubbles").style("opacity", 1)
   }
 
@@ -124,91 +125,91 @@ d3.json('./api/data').then((d) => {
     .selectAll("dot")
     .data(data)
     .join("circle")
-      .attr("class", function(d) { return "bubbles bubbles-" + Math.floor(d.rank / 39)})
-      .attr("cx", d => x(d.gni))
-      .attr("cy", d => y(d.lifeexpectancy))
-      .attr("r", d => z(d.happiness))
-      .style("fill", d => myColor(Math.floor(d.rank / 39)))
-      .attr("stroke", "black")
+    .attr("class", function (d) { return "bubbles bubbles-" + Math.floor(d.rank / 39) })
+    .attr("cx", d => x(d.gni))
+    .attr("cy", d => y(d.lifeexpectancy))
+    .attr("r", d => z(d.happiness))
+    .style("fill", d => myColor(Math.floor(d.rank / 39)))
+    .attr("stroke", "black")
     // -3- Trigger the functions for hover
-    .on("mouseover", showTooltip )
-    .on("mousemove", moveTooltip )
-    .on("mouseleave", hideTooltip )
+    .on("mouseover", showTooltip)
+    .on("mousemove", moveTooltip)
+    .on("mouseleave", hideTooltip)
 
 
 
-    // ---------------------------//
-    //       LEGEND              //
-    // ---------------------------//
+  // ---------------------------//
+  //       LEGEND              //
+  // ---------------------------//
 
-    // Add legend: circles
-    const valuesToShow = [4, 6, 8]
-    const xCircle = 1000
-    const xLabel = 1050
-    svg
-      .selectAll("legend")
-      .data(valuesToShow)
-      .join("circle")
-        .attr("cx", xCircle)
-        .attr("cy", d => height - 100 - z(d))
-        .attr("r", d => z(d))
-        .style("fill", "none")
-        .attr("stroke", "black")
+  // Add legend: circles
+  const valuesToShow = [4, 6, 8]
+  const xCircle = 1000
+  const xLabel = 1050
+  svg
+    .selectAll("legend")
+    .data(valuesToShow)
+    .join("circle")
+    .attr("cx", xCircle)
+    .attr("cy", d => height - 100 - z(d))
+    .attr("r", d => z(d))
+    .style("fill", "none")
+    .attr("stroke", "black")
 
-    // Add legend: segments
-    svg
-      .selectAll("legend")
-      .data(valuesToShow)
-      .join("line")
-        .attr('x1', d => xCircle + z(d))
-        .attr('x2', xLabel)
-        .attr('y1', d => height - 100 - z(d))
-        .attr('y2', d => height - 100 - z(d))
-        .attr('stroke', 'black')
-        .style('stroke-dasharray', ('1,1'))
+  // Add legend: segments
+  svg
+    .selectAll("legend")
+    .data(valuesToShow)
+    .join("line")
+    .attr('x1', d => xCircle + z(d))
+    .attr('x2', xLabel)
+    .attr('y1', d => height - 100 - z(d))
+    .attr('y2', d => height - 100 - z(d))
+    .attr('stroke', 'black')
+    .style('stroke-dasharray', ('1,1'))
 
-    // Add legend: labels
-    svg
-      .selectAll("legend")
-      .data(valuesToShow)
-      .join("text")
-        .attr('x', xLabel)
-        .attr('y', d => height - 100 - z(d))
-        .text( d => d)
-        .style("font-size", "10px")
-        .attr('alignment-baseline', 'middle')
+  // Add legend: labels
+  svg
+    .selectAll("legend")
+    .data(valuesToShow)
+    .join("text")
+    .attr('x', xLabel)
+    .attr('y', d => height - 100 - z(d))
+    .text(d => d)
+    .style("font-size", "10px")
+    .attr('alignment-baseline', 'middle')
 
-    // Legend title
-    svg.append("text")
-      .attr('x', xCircle)
-      .attr("y", height - 100 +30)
-      .text("Happiness Score")
-      .attr("text-anchor", "middle")
+  // Legend title
+  svg.append("text")
+    .attr('x', xCircle)
+    .attr("y", height - 100 + 30)
+    .text("Happiness Score")
+    .attr("text-anchor", "middle")
 
-    // Add one dot in the legend for each name.
-    const size = 20
-    const allgroups = [0,1,2,3,4]
-    svg.selectAll("myrect")
-      .data(allgroups)
-      .join("circle")
-        .attr("cx", 1000)
-        .attr("cy", (d,i) => 10 + i*(size+5)) // 100 is where the first dot appears. 25 is the distance between dots
-        .attr("r", 7)
-        .style("fill", d =>  myColor(d))
-        .on("mouseover", highlight)
-        .on("mouseleave", noHighlight)
+  // Add one dot in the legend for each name.
+  const size = 20
+  const allgroups = [0, 1, 2, 3, 4]
+  svg.selectAll("myrect")
+    .data(allgroups)
+    .join("circle")
+    .attr("cx", 1000)
+    .attr("cy", (d, i) => 10 + i * (size + 5)) // 100 is where the first dot appears. 25 is the distance between dots
+    .attr("r", 7)
+    .style("fill", d => myColor(d))
+    .on("mouseover", highlight)
+    .on("mouseleave", noHighlight)
 
-    // Add labels beside legend dots
-    svg.selectAll("mylabels")
-      .data(allgroups)
-      .enter()
-      .append("text")
-        .attr("x", 1000 + size*.8)
-        .attr("y", (d,i) =>  i * (size + 5) + (size/2)) // 100 is where the first dot appears. 25 is the distance between dots
-        .style("fill", d => myColor(d))
-        .text(d => `${(20*(5-d)).toString()} - ${(20*(4-d)).toString()} %`)
-        .attr("text-anchor", "left")
-        .style("alignment-baseline", "middle")
-        .on("mouseover", highlight)
-        .on("mouseleave", noHighlight)
-  })
+  // Add labels beside legend dots
+  svg.selectAll("mylabels")
+    .data(allgroups)
+    .enter()
+    .append("text")
+    .attr("x", 1000 + size * .8)
+    .attr("y", (d, i) => i * (size + 5) + (size / 2)) // 100 is where the first dot appears. 25 is the distance between dots
+    .style("fill", d => myColor(d))
+    .text(d => `${(20 * (5 - d)).toString()} - ${(20 * (4 - d)).toString()} %`)
+    .attr("text-anchor", "left")
+    .style("alignment-baseline", "middle")
+    .on("mouseover", highlight)
+    .on("mouseleave", noHighlight)
+})
